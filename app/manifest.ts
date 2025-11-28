c37792a2baa3e6197c51ed9d09db95697443b912
Chetan Sharma <156456133+chetansharma0706@users.noreply.github.com>
2025-11-28 12:05:53 +0000

    added manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+  "name": "BarberBro",
+  "short_name": "BarberBro",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "/android-chrome-192x192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "/android-chrome-512x512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ]
+  }
+}
